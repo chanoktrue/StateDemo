5c690bb9111c6970684c1f1ee5f012f6445af8ce
Thongchai Subsaidee <chanoktrue@me.com>
2023-11-08 17:31:28 +0700

    Initial Commit

---
+++ StateDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StateDemo
+//
+//  Created by Thongchai Subsaidee on 8/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StateDemo/StateDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateDemoApp.swift
+//  StateDemo
+//
+//  Created by Thongchai Subsaidee on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct StateDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
